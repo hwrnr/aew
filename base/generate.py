33e76958f4aa5a73de11fe5502a087f82e355f33
aleksa ognjanovic <alexa.ognjanovic@gmail.com>
2020-05-06 06:03:26 -0400

    Added redhat packages

--- base/generate.py
+++ base/generate.py
@@ -27,7 +27,9 @@ cursor = connection.cursor()
 cursor.execute(TABLE_QUERY.format(distro_rows))
 
 process = subprocess.Popen(distros[distro], stdout=subprocess.PIPE)
-packages = process.stdout.read().decode("utf-8").splitlines()
+if distro=="redhat":
+    packages = process.stdout.read().decode("utf-8").splitlines()
+    packages = [i.split(".")[0] for i in packages]
 
 cursor.execute("SELECT arch FROM packages")
 arch_packages = cursor.fetchall()
@@ -37,7 +39,6 @@ if distro == "arch":
         cursor.execute(ADD_ARCH_QUERY, (package, ))
 else:
     for arch_package in arch_packages:
-        print(arch_package)
         for host_package in packages:
             if arch_package[0] in host_package:
                 cursor.execute(UPDATE_QUERY.format(distro), (host_package, arch_package[0]))
