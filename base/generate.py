03b8cc0c12c7472d531ff999ab5ace03af191bd3
Aleksa Ognjanovic <alexa.ognjanovic@gmail.com>
2020-05-06 17:53:42 +0200

    Rewrited generate.py

--- base/generate.py
+++ base/generate.py
@@ -8,9 +8,9 @@ import re
 # SQLite3 queries
 ADD_TABLE_QUERY = 'CREATE TABLE IF NOT EXISTS packages (arch varchar(24));'
 ADD_ARCH_QUERY = 'INSERT INTO PACKAGES (arch) VALUES (?);'
-ADD_COLUMN_QUERY = 'ALTER TABLE packages ADD COLUMN ? varchar(24);'
+ADD_COLUMN_QUERY = 'ALTER TABLE packages ADD COLUMN {} varchar(24);'
 GET_PACKAGES_QUERY = 'SELECT arch FROM packages;'
-UPDATE_QUERY = 'UPDATE packages SET ?=? WHERE arch=?'
+UPDATE_QUERY = 'UPDATE packages SET {}=? WHERE arch=?'
 
 
 # Get packages and distro variables
@@ -25,9 +25,7 @@ elif len(sys.argv) == 2:
 else:
     sys.exit(1)
 
-
 packages = [package.replace('\n', '') for package in packages]
-
 
 # Instantiate database
 connection = sqlite3.connect('packages.db')
@@ -41,18 +39,28 @@ if distro == 'arch':
     for package in packages:
         cursor.execute(ADD_ARCH_QUERY, (package, ))
 else:
-    cursor.execute(ADD_COLUMN_QUERY, (distro, ))
+    cursor.execute(ADD_COLUMN_QUERY.format(distro))
     cursor.execute(GET_PACKAGES_QUERY)
-    for package in cursor.fetchall():
+
+    for raw_package in cursor.fetchall():
+        package = raw_package[0]
+        keywords = re.split('-|_', package)
+        alternative = None
+
         for host_package in packages:
-            if host_package in package:
-                cursor.execute(UPDATE_QUERY, (distro, host_package, package))
-        else:
-            keywords = re.split('_|-', package)
+
+            if package in host_package or host_package in package:
+                cursor.execute(UPDATE_QUERY.format(distro),
+                               (host_package, package))
+                packages.pop(packages.index(host_package))
+                break
             for keyword in keywords:
-                if keyword in package:
-                    cursor.execute(
-                        UPDATE_QUERY, (distro, host_package, package))
+                if keyword in host_package:
+                    alternative = host_package
+        else:
+            cursor.execute(UPDATE_QUERY.format(distro),
+                           (alternative, package))
+            packages.pop(packages.index(host_package))
 
 
 # Save and exit the session
